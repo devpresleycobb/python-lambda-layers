decb06946d71940445797be7999b914bb898dcf7
Presley <dev.presleycobb@gmail.com>
2021-06-13 23:05:45 -0400

    Update list_files

--- list_files/list_files/tests/unit/test_handler.py
+++ list_files/list_files/tests/unit/test_handler.py
@@ -1,7 +1,15 @@
 import pytest
 import sys
+import os
 sys.path.insert(0,'../..')
 
 
 def test_lambda_handler(event, context):
-    pytest.main(["-x", "tests/unit/modules"])
+    if 'PIPELINE' in os.environ:
+        pytest.main(["-x", "tests/unit/modules"])
+    else:
+        pytest.main(["-x", "modules"])
+
+
+if __name__ == '__main__':
+    test_lambda_handler({}, {})
